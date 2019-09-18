b2f74130b8a7126550f68634a24e4d8ea0cb1428
Павел Дмитриев <PavelDmitriev98@yandex.ru>
2019-09-18 22:34:44 +0300

    First ASP.NET core 2 application

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\ .. +",ErrorMessage = "Please enter а valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone numЬer")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you' 11 attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
